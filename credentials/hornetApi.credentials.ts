c2dc17b661c7dca7e623b6c6d6e9983899294597
JM <jmorasch@symplasson.de>
2026-02-18 13:01:58 +0000

    update

--- credentials/hornetApi.credentials.ts
+++ credentials/hornetApi.credentials.ts
@@ -1,47 +1,36 @@
 import {
-    ICredentialType,
-    INodeProperties,
+	ICredentialType,
+	INodeProperties,
 } from 'n8n-workflow';
 
 export class hornetApi implements ICredentialType {
-    name = 'hornetApi';
-    displayName = 'hornet API';
-    documentationUrl = 'https://cp.hornetsecurity.com/api/v0/docs/';
-    properties: INodeProperties[] = [
-        {
-            displayName: 'hornet API URL',
-            name: 'url',
-            placeholder: "https://cp.hornetsecurity.com",
-            type: 'string',
-            default: '',
-            required: true,
-        },
-        {
-            displayName: 'CP-TOKEN',
-            name: 'CP-TOKEN',
-            type: 'string',
-            placeholder: "00000000-0000-0000-0000-000000000000",
-            default: '',
-            required: true,
-            typeOptions: {password: true},
-        },
+	name = 'hornetApi';
+	displayName = 'ChatWoot Access Token API';
+	documentationUrl = 'https://github.com/sufficit/n8n-nodes-chatwoot/docs/auth';
+	properties: INodeProperties[] = [
 		{
-            displayName: 'APP-VERSION',
-            name: 'APP-VERSION',
-            type: 'string',
-            placeholder: "00000000-0000-0000-0000-000000000000",
-            default: '',
-            required: true,
-            typeOptions: {password: true},
-        },
-	{
-	    displayName: 'Authorization',
-            name: 'Authorization',
-            type: 'string',
-            placeholder: "00000000-0000-0000-0000-000000000000",
-            default: '',
-            required: true,
-            typeOptions: {password: true},
-        },
-    ];
+			displayName: 'ChatWoot API Url',
+			name: 'baseUrl',
+			placeholder: "https://www.chatwoot.com",
+			type: 'string',
+			default: '',
+			required: true,
+		},
+		{
+			displayName: 'Access Token',
+			name: 'accessToken',
+			type: 'string',
+			placeholder: "00000000-0000-0000-0000-000000000000",
+			default: '',
+			required: true,
+		},
+		{
+			displayName: 'Account Id',
+			name: 'accountId',
+			type: 'string',
+			placeholder: "1",
+			default: '1',
+			required: true,
+		},
+	];
 }
